2b0c62de3f849480c23962cf733bb067e02fff87
csophys <zjsy_cs@163.com>
2016-04-01 22:44:02 +0800

    订单报名信息表http接口 ok & unit ok

--- src/main/java/org/csophys/earthworker/web/RegistrationController.java
+++ src/main/java/org/csophys/earthworker/web/RegistrationController.java
@@ -1,11 +1,15 @@
 package org.csophys.earthworker.web;
 
+import org.csophys.earthworker.web.entity.Registration;
 import org.csophys.earthworker.web.service.RegistrationService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * Created by csophys on 16/3/31.
@@ -17,9 +21,37 @@ public class RegistrationController {
     @Resource
     RegistrationService registrationService;
 
-    @RequestMapping("")
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
     @ResponseBody
-    public String getRegistration() {
-        return "success";
+    public String getRegistration(Registration registration) {
+        int result = registrationService.insert(registration);
+        if (result > 0) {
+            return result + "";
+        } else {
+            return Constant.FAIL;
+        }
+    }
+
+    @RequestMapping(value = "/{id}")
+    @ResponseBody
+    public Registration getById(@PathVariable("id") int id) {
+        return registrationService.getById(id);
+    }
+
+    @RequestMapping(value = "/getByWeixinId")
+    @ResponseBody
+    public List<Registration> getByWeixinId(String weixinId) {
+        return registrationService.getByField("weixinId", weixinId);
+    }
+
+    @RequestMapping(value = "/update", method = RequestMethod.POST)
+    @ResponseBody
+    public String updateRegistration(Registration registration) {
+        int result = registrationService.updateById(registration.getId(), registration);
+        if (result > 0) {
+            return Constant.SUCCESS;
+        } else {
+            return Constant.FAIL;
+        }
     }
 }
